7d340e18efbb0266bdf2249bf7cf56178d57f639
Abbi <33093801+lesbiangunshow@users.noreply.github.com>
2021-06-19 22:32:36 +0100

    part 15

--- src/main/kotlin/com/abbisea/caves/attributes/ItemCombatStats.kt
+++ src/main/kotlin/com/abbisea/caves/attributes/ItemCombatStats.kt
@@ -1,4 +1,18 @@
 package com.abbisea.caves.attributes
 
-class ItemCombatStats {
+import org.hexworks.amethyst.api.base.BaseAttribute
+import org.hexworks.zircon.api.Components
+import org.hexworks.zircon.api.component.Component
+
+data class ItemCombatStats(
+    val attackValue: Int = 0,
+    val defenseValue: Int = 0,
+    val combatItemType: String
+) : BaseAttribute(), DisplayableAttribute {
+
+    override fun toComponent(width: Int): Component = Components.textBox(width)
+        .addParagraph("Type: $combatItemType", withNewLine = false)
+        .addParagraph("Attack: $attackValue", withNewLine = false)
+        .addParagraph("Defense: $defenseValue", withNewLine = false)
+        .build()
 }
